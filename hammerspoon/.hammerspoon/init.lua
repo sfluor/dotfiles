149b0843281f5c2ed431ef6ee02321ee2cb1ff6a
Sami Tabet <sami.tabet@datadoghq.com>
2023-06-15 12:51:49 +0200

    hammerspoon: update config to play better with two browsers

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,4 +1,4 @@
-function runOrFocus(name) 
+function runOrFocus(name)
     local launched = hs.application.launchOrFocus(name)
     if (not launched) then
         hs.alert.show(name .. " not found!")
@@ -7,26 +7,35 @@ end
 
 -- Creates a shortcut to quickly open the given application
 function mapApplication(name, key)
-    hs.hotkey.bind({"alt"}, key, function()
-        local app = hs.appfinder.appFromName(name)
-        if (name ~= nil) then
-            -- If we found the app, let's check if it's already focused
-            local window = app:focusedWindow()
-            if (window ~= nil) then
-                -- If it's not focused yet, let's run and focus on it
-                runOrFocus(name)
-            else
-                -- Otherwise let's just focus on it.
-                window:focus()
-            end
-        else
-            -- Otherwise let's launch it
-            runOrFocus(name)
-        end
-       
-      end)
+    hs.hotkey.bind({ "alt" }, key, function()
+        runOrFocus(name)
+    end)
 end
 
-mapApplication("Firefox", "B")
+hs.hotkey.bind({ "alt" }, "R", function()
+    hs.reload()
+end)
+
+hs.hotkey.bind({ "alt", "shift" }, "M", function()
+    local window = hs.window.focusedWindow()
+    if (window ~= nil) then
+        window:toggleFullScreen()
+    else
+        hs.alert.show("No focused window, can't toggle full screen.")
+    end
+end)
+
+-- Prefer using Firefox if it's installed (ideally we should grab the default browser from the config)
+BROWSER = "Google Chrome"
+if (hs.application.infoForBundlePath("/Applications/Firefox.app") ~= nil) then
+    BROWSER = "Firefox"
+end
+
+mapApplication("GoLand", "G")
+mapApplication("IntelliJ IDEA CE", "I")
+mapApplication("Postman", "P")
+mapApplication(BROWSER, "B")
 mapApplication("Alacritty", "T")
 mapApplication("Obsidian", "O")
+
+hs.alert.show("Config reloaded")
